bd55fc60f36611ea24fc9a40dc1f0bf9e980ee62
Xiafyjck <Xiafyjck@outlook.com>
2021-09-28 22:26:05 +0800

    rand

--- script.js
+++ script.js
@@ -7,18 +7,33 @@ const an=document.getElementById("an")
 function sleep(time){
     return new Promise((resolve)=>setTimeout(resolve,time));
 }
+
+//乱序
+sortedl=[]
+randl=[]
+j=data.length;
+for(i=0;i<50;i++){
+    j--;
+    sortedl[i]=j;
+}
+for(i=0;i<data.length-50;i++){
+    randl[i]=i;
+}
+randl.sort(()=>Math.random()>0.5?-1:1)
+randarr=sortedl.concat(randl)
+
 count=0;
 function f(){
-    word.textContent=data[count]["name"];
+    word.textContent=data[randarr[count]]["name"];
    next.setAttribute("width","10px")
-   quotetrans.textContent=data[count]["trans"];
+   quotetrans.textContent=data[randarr[count]]["trans"];
    str='';
-   for(i in data[count]["notes"]){
-       str+=data[count]["notes"][i]
+   for(i in data[randarr[count]]["notes"]){
+       str+=data[randarr[count]]["notes"][i]
    };
    quotenotes.textContent=str;
 
-   audio.src="http://media.shanbay.com/audio/us/"+data[count]["name"]+".mp3";
+   audio.src="http://media.shanbay.com/audio/us/"+data[randarr[count]]["name"]+".mp3";
    audio.play();
    setTimeout('audio.play()','1500');
    count++;
